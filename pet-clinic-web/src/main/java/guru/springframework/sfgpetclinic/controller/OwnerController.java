5a7b260a46e24c98da4e3953a5d93e45f78f70f7
chy.asiful@gmail.com <chy.asiful@gmail.com>
2020-04-04 17:55:00 -0400

    Add owner list and update controller

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -1,13 +1,26 @@
 package guru.springframework.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.services.OwnerService;
 
 @Controller
 public class OwnerController {
 	
+	private final OwnerService ownerService;
+	
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({"/owners","/owners/index","/owners/index.html"})
-	public String listOwner() {
+	public String listOwner(Model model) {
+		
+		model.addAttribute("owners", ownerService.findAll());
+		
 		return "owners/index";
 	}
 
