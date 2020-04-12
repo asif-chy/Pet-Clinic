48dc51007343eb9abea5de3dd756933aaf2d7fc9
chy.asiful@gmail.com <chy.asiful@gmail.com>
2020-04-12 13:57:53 -0400

    Added visit, updated owner.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -1,10 +1,22 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
 	public Owner() {
 		super();
 		// TODO Auto-generated constructor stub
+	}
+	
+	private Set<Pet> pets;
+
+	public Set<Pet> getPets() {
+		return pets;
+	}
+
+	public void setPets(Set<Pet> pets) {
+		this.pets = pets;
 	}
 
 }
