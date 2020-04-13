35091af85106602e3932845f6224e7a8dfb5ad7b
chy.asiful@gmail.com <chy.asiful@gmail.com>
2020-04-13 16:00:51 -0400

    Implement speciality map service

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/SpecialityServiceMap.java
@@ -0,0 +1,31 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import java.util.Set;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.services.SpecialityService;
+
+public class SpecialityServiceMap extends AbstractMapService<Speciality, Long> implements SpecialityService{
+
+	public Set<Speciality> findAll(){
+		return super.findAll();
+	}
+	
+	public Speciality findById(Long id) {
+		return super.findById(id);
+	}
+	
+	public Speciality save(Speciality speciality) {
+		return super.save(speciality);
+	}
+	
+	public void delete(Speciality speciality) {
+		super.delete(speciality);
+	}
+
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+	
+}
