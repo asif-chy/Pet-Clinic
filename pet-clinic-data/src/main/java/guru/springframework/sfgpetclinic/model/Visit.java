e31e3b18ea3952aed1e29209ab08c7161ffb3843
chy.asiful@gmail.com <chy.asiful@gmail.com>
2020-05-02 17:02:54 -0400

    Vist JPA Annotations added

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Visit.java
@@ -2,12 +2,27 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="visit")
 public class Visit {
 	
+	
 	private Pet pet;
+	
+	@Column(name="Date")
 	private LocalDate date;
+	
+	@Column(name="Description")
 	private String description;
 	
+	@ManyToOne
+	@JoinColumn(name="pet_id")
 	public Pet getPet() {
 		return pet;
 	}
