55e7adeaa41987fc231beba1c27039ecd1cf6e8c
chy.asiful@gmail.com <chy.asiful@gmail.com>
2020-05-02 16:47:55 -0400

    Update Vet/Specialty with JPA annotations.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -2,9 +2,20 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.JoinColumn;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 
+@Entity
+@Table(name="vet")
 public class Vet extends Person {
 
+	@ManyToMany(fetch= FetchType.EAGER)
+	@JoinTable(name="vet_specialties", joinColumns = @JoinColumn(name="vet_id"),
+	inverseJoinColumns = @JoinColumn(name="specialty_id"))
 	private Set<Speciality> speciality = new HashSet<>();
 
 	public Set<Speciality> getSpeciality() {
